be148ca0a101104a6f91fad43a6bcfa9a5da8684
furkancinarz <cinarfurkan413@gmail.com>
2023-05-06 14:26:45 +0300

    commit tamamlandi2

--- src/test.java
+++ src/test.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main {
+public class test {
     public static void main(String[] args){
         Scanner inp = new Scanner(System.in);
         System.out.print("Alınan para değerini giriniz:");
